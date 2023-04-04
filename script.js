a5b623edda8df0a0c8fc7a6998f5b3724e61838f
Matt West <workmailmattwest@gmail.com>
2023-04-04 21:21:06 +0200

    Added buttons
    Buttons change the color of the page using js

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeColorRed() {
+  document.body.style.backgroundColor = "red";
+}
+function changeColorBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+function changeColorGreen() {
+  document.body.style.backgroundColor = "green";
+}
+function changeColorWhite() {
+  document.body.style.backgroundColor = "white";
+}
+function changeColorPink() {
+  document.body.style.backgroundColor = "pink";
+}
