10e1d36d890a807443132a76e9f52f154581d891
Matt West <workmailmattwest@gmail.com>
2023-04-06 22:33:43 +0200

    Added state functionality
    UI now mirrors data from the app state.
    
    UI and Data are seperate

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
 const totalPriceElement = document.getElementById("totalPrice");
 const parentListElement = document.getElementById("list");
+
 let totalPrice = 0;
+let cart = [];
+
+class Item {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+function refreshUI() {
+  totalPriceElement.innerText = `Total price: $${totalPrice}`;
+  parentListElement.innerHTML = "";
+  cart.forEach((item, index) => {
+    const listElement = document.createElement("li");
+    const textNode = document.createTextNode(`${item.name} - ${item.price}`);
+    listElement.appendChild(textNode);
+    parentListElement.appendChild(listElement);
+    listElement.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between"
+    );
+    const deleteButton = document.createElement("button");
+    const deleteTextNode = document.createTextNode("Delete");
+    deleteButton.appendChild(deleteTextNode);
+    deleteButton.classList.add("btn", "btn-danger");
+    listElement.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", () => {
+      cart.splice(index, 1);
+      totalPrice -= item.price;
+      refreshUI();
+    });
+  });
+}
+
 function addItem(form) {
   const itemName = form.itemName.value;
   const itemPrice = form.itemPrice.value;
 
   totalPrice += parseInt(itemPrice);
-  totalPriceElement.innerText = `Total price: $${totalPrice}`;
-  const listElement = document.createElement("li");
-  const textNode = document.createTextNode(`${itemName} - ${itemPrice}`);
-  listElement.appendChild(textNode);
-  parentListElement.appendChild(listElement);
-  listElement.classList.add(
-    "list-group-item",
-    "d-flex",
-    "justify-content-between"
-  );
-  const deleteButton = document.createElement("button");
-  const deleteTextNode = document.createTextNode("Delete");
-  deleteButton.appendChild(deleteTextNode);
-  deleteButton.classList.add("btn", "btn-danger");
-  listElement.appendChild(deleteButton);
+  const item = new Item(itemName, parseInt(itemPrice));
+  cart.push(item);
 
-  deleteButton.addEventListener("click", () => {
-    listElement.remove();
-    totalPrice -= parseInt(itemPrice);
-    totalPriceElement.innerText = `Total Price: $${totalPrice}`;
-  });
+  refreshUI();
   return false;
 }
